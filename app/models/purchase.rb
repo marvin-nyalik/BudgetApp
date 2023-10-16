c02651e6e8cb3a781d93eb34b7edb147c39fb709
MARVIN <o.silverygold@gmail.com>
2023-10-16 15:31:18 +0300

    Update model associations

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,5 +3,5 @@ class Purchase < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :categories
+  has_many :categories, through: :category_purchases
 end
